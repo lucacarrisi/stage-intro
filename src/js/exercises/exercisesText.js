8525338c090834c864c1c9a9bf17dddcef19515a
luca.carrisi <luca.carrisi@ccenture.com>
2024-07-03 16:17:22 +0200

    update js

--- src/js/exercises/exercisesText.js
+++ src/js/exercises/exercisesText.js
@@ -1,11 +1,18 @@
-//1. Creare un array di 25 numeri, e copiarlo in un secondo array, duplicando gli elementi in posizione pari e dimezzando quelli in posizione dispari; (stampare gli array)
+//Math.random() -> [0, 1] inclusi i decimali
+//Math.floor(Math.random()) -> 0, 1
+//Math.flor(Math.random() * 201) -> [0, 200] solo gli interi
+//Math.floor(Math.random() * 201) - 100) -> crea l'intervallo [0, 200] e lo sposta nell'intervallo [-100, 100]
 
-//2. Creare un array di N numeri, l'inserimento si chiude quando viene inserito il numero -1, stampare array e lunghezza array;
+//1. Creare un array di 25 numeri casuali tra -100 e 100, e copiarlo in un secondo array, duplicando gli elementi in posizione pari e dimezzando quelli in posizione dispari; (stampare gli array)
 
-//3. Creare un array di 10 oggetti, dove ogni oggetto deve contenere le chiavi 'name', 'age', restituire il numero di oggetti con 'age' >= 18;
+//2. Creare un array di N numeri casuali tra 0 e 100, l'inserimento si chiude quando viene inserito il numero -1, stampare array e lunghezza array;
+
+//3. Creare un array di 5 oggetti, dove ogni oggetto deve contenere le chiavi 'name', 'age', restituire il numero di oggetti con 'age' >= 18; !N.B. l'età può essere compresa tra 0 e 99
 
 //4. Dato un array di oggetti
 //   [{value: -34, key: 'a'}, {value: 2, label: 'b'}, {value: 5, label: 'c'}, {value: -5, label: 'c'}, {value: 2, label: 'c'}, {value: 1, label: 'a'}, {value: 3, label: 'a'}, {value: -102, label: 'b'}, {value: 9, label: 'c'}, {value: -3, label: 't'}]
 //   creare n oggetti quante sono le chiavi, e sommando tutti i value con le stesse chiavi es [{key: 'a', value: '54'},...]
+
+//5.
 
 
