b3b2e3c7b0b29c43633882f3a0f5c8e491d7b753
Petrioli <flavia.petrioli@accenture.com>
2024-07-03 14:35:18 +0200

    update Incentivi

--- src/js/exercises/first.js
+++ src/js/exercises/first.js
@@ -211,5 +211,5 @@ export default function First(){
         }
     })
 
-    console.log(myArray, secondArray, thirdArray, fourthArray, fifthArray, value, array71, valueIndex, myArray, trovato, final, finalValues, secondValues, chiamateValues, chiamate, stringVal)
+    console.log(myArray, secondArray, thirdArray, fourthArray, fifthArray, value, array71, valueIndex, myArray, trovato, final, finalValues, secondValues, chiamateValues, chiamate)
 }
